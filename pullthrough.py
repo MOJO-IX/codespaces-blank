2d7618d073ab4ca392d6591935290332036c4338
mahajoub gargab <160452700+MOJO-IX@users.noreply.github.com>
2024-03-07 21:44:27 +0300

    Add files via upload

---
+++ pullthrough.py
@@ -0,0 +1,10 @@
+import random
+print("welcome welcome")
+a = random.randint(0, 9)
+b = random.randint(0, 9)
+print(f"you got {a}")
+print(f"pc got {b}")
+
+print(f"you got {a+a}")
+print(f"pc got {b+b}")
+
